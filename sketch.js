0babde85a1c704d0f238e06a6d2dba3c45200227
harshal2580 <75899048+harshal2580@users.noreply.github.com>
2020-12-19 19:41:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(800,400);
+  movingRect=createSprite(400, 200, 80, 30);
+  movingRect.shapeColor = "red";
+  movingRect.debug = true;
+
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = "red";
+  fixedRect.debug = true;
+} 
+
+function draw() {
+  background(0);
+
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x - fixedRect.x <= movingRect.width/2 + fixedRect.width/2 && 
+     fixedRect.x - movingRect.x <= movingRect.width/2 + fixedRect.width/2 && 
+      movingRect.y - fixedRect.y <= movingRect.height/2 + fixedRect.height/2 && 
+       fixedRect.y - movingRect.y <= movingRect.height/2 + fixedRect.height/2) {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  else{
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  drawSprites();
+}
